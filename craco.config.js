90ccfa8d311099c7a842e96136321200a646224b
idtpladmn <idtpl.admn@gmail.com>
2026-08-13 07:10:07 +0530

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,10 @@
-const { POSTCSS_MODES } = require("@craco/craco");
-
 module.exports = {
   style: {
     postcss: {
-      mode: POSTCSS_MODES.file,
+      plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+      ],
     },
   },
 };
