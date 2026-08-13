427579c196262b2824b30304d55c5a92869a9d6b
idtpladmn <idtpl.admn@gmail.com>
2026-08-13 06:59:27 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: "class",
 
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -94,7 +94,5 @@ module.exports = {
     },
   },
 
-  plugins: [
-    require("tailwindcss-animate"),
-  ],
+  plugins: [require("tailwindcss-animate")],
 };
